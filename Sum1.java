86edfb6c0f705c6c54ca34652bb758fc728e080a
GanasalaChandana <48092343+GanasalaChandana@users.noreply.github.com>
2020-01-28 15:29:26 +0530

    Add files via upload

---
+++ Sum1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum1
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int n=sc.nextInt();
+int sum=m+n;
+System.out.println("sum of 2 numbers:"+sum);
+}
+}
